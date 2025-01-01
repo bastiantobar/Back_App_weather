7483a496f631ca518d5ddf3cff72c33c8cb385a6
Bastian Tobar Mori <bastiantobar.94@gmail.com>
2025-01-01 12:29:03 +0100

    first commit

---
+++ src/main/java/Proyecto/back/para/aplicacion/movil/de/TFM/RestAppWeatherApplication.java
@@ -0,0 +1,13 @@
+package Proyecto.back.para.aplicacion.movil.de.TFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAppWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proyecto/back/para/aplicacion/movil/de/TFM/RestAppWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto.back.para.aplicacion.movil.de.TFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
