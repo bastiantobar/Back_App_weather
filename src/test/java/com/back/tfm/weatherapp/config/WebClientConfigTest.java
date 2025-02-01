a8b89ed62618c393eb37d68d20dc0c0cb91e2a04
Bastian <bastiantobar.94@gmail.com>
2025-02-01 01:14:37 +0100

    Create WebClientConfigTest.java

---
+++ src/test/java/com/back/tfm/weatherapp/config/WebClientConfigTest.java
@@ -0,0 +1,42 @@
+package com.back.tfm.weatherapp.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class WebClientConfigTest {
+
+    @InjectMocks
+    private WebClientConfig webClientConfig;
+
+    @Mock
+    private WebClient.Builder webClientBuilderMock;
+
+    @Mock
+    private WebClient webClientMock;
+
+    @BeforeEach
+    void setUp() {
+        when(webClientBuilderMock.baseUrl("https://api.met.no/weatherapi/")).thenReturn(webClientBuilderMock);
+        when(webClientBuilderMock.defaultHeader("User-Agent", "MyWeatherApp/1.0 (bastiantobar@example.com)")).thenReturn(webClientBuilderMock);
+        when(webClientBuilderMock.build()).thenReturn(webClientMock);
+    }
+
+    @Test
+    void testWebClientBeanCreation() {
+        WebClient webClient = webClientConfig.webClient(webClientBuilderMock);
+
+        assertNotNull(webClient);
+        verify(webClientBuilderMock, times(1)).baseUrl("https://api.met.no/weatherapi/");
+        verify(webClientBuilderMock, times(1)).defaultHeader("User-Agent", "MyWeatherApp/1.0 (bastiantobar@example.com)");
+        verify(webClientBuilderMock, times(1)).build();
+    }
+}
